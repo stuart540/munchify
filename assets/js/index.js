2441ff26623535d7714eb106eb983f78827c06e4
Sam Callaway <calls002@gmail.com>
2023-02-02 20:47:34 +0000

    Written function for saving recipe to storage when favourite button is clicked. Still need to add some details

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,9 @@ var recipeContainer = $("#recipeContainer");
 var landingContainer = $("#landingContainer");
 
 var randomiseButton = $("#randomiseButton");
+var favouriteRecipeButton = $("#favouriteRecipe");
 
-getRecipes("italian");
+
 
 function getRecipes(cuisine){
     var queryURL = "https://api.spoonacular.com/recipes/random?apiKey=ad7eae9455534c2b9df85e498cdf804b&number=1&tags=dinner,main dish,"+cuisine
@@ -31,6 +32,7 @@ function getRecipes(cuisine){
             recipeImageURL : response.recipes[0].image,
             recipeInstructions : response.recipes[0].instructions,
             recipeURL : response.recipes[0].sourceUrl,
+            recipeID : response.recipes[0].id
         }
         
         console.log(fullRecipe);
@@ -49,3 +51,29 @@ var hideLanding = function () {
 randomiseButton.on("click",hideLanding);
 
 
+// Save current recipe to local storage when favourite button is clicked. Or, remove from local storage if clicked again.
+// When we initially generate the favourites button we need to check whether the recipe ID is already present in local storage. Then, set the data-saved attribute to true if it is or false if not
+
+favouriteRecipeButton.on('click', function(){
+if(favouriteRecipeButton.attr(data-saved) === false)
+{
+    favouriteRecipeButton.attr(data-saved, true) 
+    // Need to add a line here to update styling/text content on button to show the recipe is saved
+    
+    
+    // Adding the recipe into local storage using the ID from spoonacular as the name.
+    // Using the spoonacular ID gives us a unique ID for each recipe so we can easily check if it already exists 
+    // Define currentRecipe as a global variable elsewhere. It should be an object matching the structure of the fullRecipe object in getRecipes()
+    var savingRecipe = JSON.stringify(currentRecipe);
+    localStorage.setItem(currentRecipe.recipeID, savingRecipe);
+}
+else
+{
+    favouriteRecipeButton.attr(data-saved, true);
+    localStorage.removeItem(currentRecipe.recipeID);
+
+    // Need to add a line here to update styling/text content on button to show the recipe is not saved
+}
+
+}
+)
