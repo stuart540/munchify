0338d450e8628109e70bfacf9cea6502ab0c0362
Sam Callaway <calls002@gmail.com>
2023-02-02 19:10:48 +0000

    Finished first version of API call to return results as JSON string

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,8 +12,25 @@ function getRecipes(cuisine){
     method: "GET"
   })
     .then(function(response) {
-    
+
+        var ingredients = []
+        response.recipes[0].extendedIngredients.forEach(element => {
+            ingredients.push(element.original)
+        });
 
 
+        var fullRecipe = {
+            recipeName : response.recipes[0].title,
+            recipeIngredients : ingredients,
+            recipeImageURL : response.recipes[0].image,
+            recipeInstructions : response.recipes[0].instructions,
+            recipeURL : response.recipes[0].sourceUrl,
+        }
+        output = JSON.stringify(fullRecipe)
+        return(fullRecipe)
     });
 }
+
+
+
+
